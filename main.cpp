b2059e5f78c2aaf06aed42e6411b489bfdd1a81d
FuLeZi <florian.golestin@gmail.com>
2016-05-01 19:01:00 +0200

    Clean Code

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 #include <osgViewer/Viewer>
 #include <osg/LightSource>
 #include <osgGA/FirstPersonManipulator>
-
-#include <osg/PositionAttitudeTransform>
+#include <osg/AnimationPath>
+#include <osgGA/KeySwitchMatrixManipulator>
+#include <osgGA/TrackballManipulator>
 
 #include <osg/io_utils>
 
@@ -17,31 +18,6 @@
 #include <osg/io_utils>
 
 
-// #include <osg/Texture2D>
-// namespace Soleil
-// {
-//   class BlenderCube : public Mesh
-//   {
-//   public:
-//     BlenderCube() {
-//       osg::ref_ptr<osg::Node> model =
-// 	osgDB::readNodeFile("/usr/home/florian/Documents/Jeux/futur/cube.obj");
-//       if (!model)
-// 	  throw "Cannot read model";
-//       osg::ref_ptr<osg::Group> group = new osg::Group;
-//       group->addChild(model);
-//       this->addChild(group);
-//       osg::ref_ptr<osg::Texture2D> texture = new osg::Texture2D;
-//       osg::ref_ptr<osg::Image> image = osgDB::readImageFile("/usr/home/florian/Documents/Jeux/futur/stone_3_2048x2048.jpg");
-//       texture->setImage(image);
-//       texture->setUnRefImageDataAfterApply(true);
-//       texture->setDataVariance(osg::Object::DYNAMIC);
-//       this->getOrCreateStateSet()->setTextureAttributeAndModes(0, texture);
-//     }
-//     virtual ~BlenderCube(void) {}    
-//   };
-// };
-
 namespace Soleil
 {
   class Object : public osg::PositionAttitudeTransform
@@ -49,7 +25,7 @@ namespace Soleil
   public:
     Object() {
       osg::ref_ptr<osg::Node> model =
-	osgDB::readNodeFile("/usr/home/florian/jeux/OpenSceneGraph-Data/axes.osgt");
+	osgDB::readNodeFile("media/axes.osgt");
       if (!model)
 	  throw "Cannot read model";
       osg::ref_ptr<osg::Group> group = new osg::Group;
@@ -91,11 +67,10 @@ osg::ref_ptr<osg::Node> createLightSource( unsigned int num,
 
 int	main(int argc, char **argv)
 {
-  osgViewer::Viewer viewer;
+  osg::ArgumentParser arguments(&argc,argv);
   
   osg::ref_ptr<osg::Group> root = new osg::Group;
 
-  //root->addChild(new Soleil::Cube());
   root->addChild(createLightSource(0, osg::Vec3(0, -2.0, 0), osg::Vec4(1.0, 1.0, 1.0, 1.0)));
   root->addChild(createLightSource(1, osg::Vec3(0, -16.0, 10.0), osg::Vec4(1.0, 1.0, 1.0, 1.0)));
 
@@ -103,21 +78,22 @@ int	main(int argc, char **argv)
   root->getOrCreateStateSet()->setMode( GL_LIGHT1, osg::StateAttribute::ON );
 
 
-  // osg::ref_ptr<Soleil::Mesh> cube = new Soleil::Cube();
-  // cube->setPosition(osg::Vec3(1.0, 0.0, 0.0));
-  // root->addChild(cube);
-
-
-  // osg::ref_ptr<Soleil::Mesh> cube = new Soleil::BlenderCube();
-  // cube->setPosition(osg::Vec3(3.0, 0.0, 0.0));
-  // root->addChild(cube);
-
   Soleil::LevelReader l;
   osg::ref_ptr<Soleil::Level>  level;
   try
     {
+      std::string file;
+
+      if (!arguments.read("-l", osg::ArgumentParser::Parameter(file)))
+	level = l.readFile("media/bastion.level");
+      else
+	level = l.readFile(file);
+
       // level = l.readFile("/usr/home/florian/Documents/Jeux/futur/axes.level");
-      level = l.readFile("/usr/home/florian/Documents/Jeux/futur/large.level");
+      //level = l.readFile("/usr/home/florian/Documents/Jeux/futur/large.level");
+      //level = l.readFile("/usr/home/florian/Documents/Jeux/futur/ouvert.level");
+      //level = l.readFile("/usr/home/florian/Documents/Jeux/futur/bastion.level");
+      //level = l.readFile("/usr/home/florian/Documents/Jeux/futur/huge.level");
       if (!level)
   	throw "Cannot read model";
       root->addChild(level);
@@ -127,17 +103,15 @@ int	main(int argc, char **argv)
       std::cout << "Read model failed: "<< e.what() << "=>" << ::strerror(errno)  << "\n";
       return 1;
     }
+  catch (const char *msg)
+    {
+      std::cout << "Read model failed: "<< msg  << "\n";
+      return 1;
+    }
   
+
   
-  viewer.setSceneData(root);
-
-  // std::cout << "Starting Position: " << level->startingPosition() << ". Orientation: " << level->startingOrientation() <<  "\n";
-  // viewer.getCamera()->setProjectionMatrixAsPerspective(40., 1., .1, 1000.);
-
-
-
-
-    //////////
+  //////////
   // TEST //
   //////////
   osg::ref_ptr<Soleil::Object> obj = new Soleil::Object;
@@ -155,17 +129,15 @@ int	main(int argc, char **argv)
   
 
   /// TEST ON CAMERA
-  // osg::Vec3d normale(0.0, 0.0, 1.0); 
-  
   // viewer.getCamera()->setClearColor( osg::Vec4( 0., 0., 0., 0. ) );  
 
-  // osg::ref_ptr<Soleil::FirstPersonManipulator> caman = new Soleil::FirstPersonManipulator(level->startingPosition(), level->startingOrientation());
-  // viewer.addEventHandler(caman);
-  // osg::Vec3d eye = level->startingPosition();
-  // osg::Vec3d center = level->startingOrientation();
+  osg::ref_ptr<osgViewer::Viewer>  viewer = new osgViewer::Viewer;
+  viewer->setSceneData(root);
 
-  // osgGA::FirstPersonManipulator doomView;  
-  viewer.setCameraManipulator(new Soleil::FirstPersonManipulator(level->startingPosition(), level->startingOrientation()));
+  if (!arguments.read("-d"))
+    viewer->setCameraManipulator(new Soleil::FirstPersonManipulator(level->startingPosition(), level->startingOrientation()));
+  /* else auto-add trackball */
+  
     
   // while (!viewer.done())
   //   {
@@ -182,5 +154,5 @@ int	main(int argc, char **argv)
 
 
   
-  return viewer.run();
+  return viewer->run();
 }
